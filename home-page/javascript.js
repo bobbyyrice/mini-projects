1cc2bea999353945906c0f9d1330ed4efa47179f
bobby <kirboepik@gmail.com>
2024-08-04 18:02:14 -1000

    add minor changes

--- home-page/javascript.js
+++ home-page/javascript.js
@@ -11,3 +11,35 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// bla bla bla
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show2');
+        } else {
+            entry.target.classList.remove('show2');
+        }
+    });
+});
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el) => observer2.observe(el));
+
+// bla bla bla
+
+const observer3 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show3');
+        } else {
+            entry.target.classList.remove('show3');
+        }
+    });
+});
+
+const hiddenElements3 = document.querySelectorAll('.hidden3');
+hiddenElements3.forEach((el) => observer3.observe(el));
