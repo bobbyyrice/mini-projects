0e2bbb5d358952f56fdeaef8ab44d86117510a24
bobbyyrice <kirboepik@gmail.com>
2024-08-10 12:03:43 -1000

    sdfsdf

--- mainSite/aboutus/javascript.js
+++ mainSite/aboutus/javascript.js
@@ -25,3 +25,17 @@ const observer2 = new IntersectionObserver((entries) => {
 
 const hiddenElements2 = document.querySelectorAll('.hidden2');
 hiddenElements2.forEach((el) => observer2.observe(el));
+
+const observer3 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show3');
+        } else {
+            entry.target.classList.remove('show3');
+        }
+    });
+});
+
+const hiddenElements3 = document.querySelectorAll('.hidden3');
+hiddenElements3.forEach((el) => observer3.observe(el));
