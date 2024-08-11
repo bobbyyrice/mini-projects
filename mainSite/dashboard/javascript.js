9343d8ea4526cfe899d99fbfc6a80a738354d736
bobbyyrice <kirboepik@gmail.com>
2024-08-10 19:15:51 -1000

    add some stuff idk

--- mainSite/dashboard/javascript.js
+++ mainSite/dashboard/javascript.js
@@ -9,3 +9,41 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+// bla bla bla
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show2');
+        }
+    });
+});
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el) => observer2.observe(el));
+// bla bla bla
+const observer3 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show3');
+        } else {
+            entry.target.classList.remove('show3')
+        }
+    });
+});
+
+const hiddenElements3 = document.querySelectorAll('.hidden3');
+hiddenElements3.forEach((el) => observer3.observe(el));
+
+const observer4 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show4');
+        }
+    });
+});
+
+const hiddenElements4 = document.querySelectorAll('.hidden4');
+hiddenElements4.forEach((el) => observer4.observe(el));
